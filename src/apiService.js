228027cfe68f1ed5c4c7b12b3876d86793ae934a
PrasaathIBE <prasaathibe@gmail.com>
2024-08-30 10:54:08 +0530

    Initial commit

---
+++ src/apiService.js
@@ -0,0 +1,9 @@
+import axios from 'axios';
+
+const API_BASE_URL = 'https://cors-dashboard-backend-omega.vercel.app/';
+
+const sendJsonData = (inputData) => {
+    return axios.post(`${API_BASE_URL}/api/json/`, { input: inputData });
+};
+
+export default sendJsonData;
