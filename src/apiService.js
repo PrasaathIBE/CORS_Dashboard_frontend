85a7e12481443f0e78ca3b374f21ab1ad84c1de1
PrasaathIBE <prasaathibe@gmail.com>
2024-08-30 11:31:02 +0530

    URL change

--- src/apiService.js
+++ src/apiService.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const API_BASE_URL = 'https://cors-dashboard-backend-omega.vercel.app/';
+const API_BASE_URL = 'https://cors-dashboard-backend-omega.vercel.app';
 
 const sendJsonData = (inputData) => {
     return axios.post(`${API_BASE_URL}/api/json/`, { input: inputData });
